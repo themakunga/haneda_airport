aeefc33472173549d28898b5f7c1f35a1e069a97
Nicolas Martinez V <nmartinezv@icloud.com>
2025-03-18 23:35:46 -0300

    feat(nix): homebrew - add packages and config cleanup
    mas
    magnet
    google-chrome *sights*

--- nix/modules/homebrew/casks-brews.nix
+++ nix/modules/homebrew/casks-brews.nix
@@ -4,12 +4,20 @@
       "nvm"
       "tfenv"
       "pnpm"
+      "mas"
     ];
     casks = [
       "figma"
       "firefox@developer-edition"
       "firefox"
       "chromium"
+      "google-chrome"
     ];
+    masApps = {
+      "Magnet" = 441258766;
+    };
+    onActivation = {
+      cleanup = "zap";
+    };
   };
 }
