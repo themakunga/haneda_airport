b280da4e16e8e2dbd9fcb3c99994e5b336e55dc6
Nicolas Martinez <nicolas@outer-heaven.local>
2025-09-06 10:30:27 -0400

    feat: kanagawa packagesz

--- hosts/darwin/kanagawa.nix
+++ hosts/darwin/kanagawa.nix
@@ -1,13 +1,27 @@
-{pkgs, self, ...}: {
+{...}: {
   imports = [
     ../../packages/colima.nix
     ../../packages/browsers.nix
     ../../packages/development.nix
     ../../packages/document.nix
     ../../packages/fonts.nix
-    ../../packages/games.nix
     ../../packages/social.nix
     ../../packages/terminal.nix
     ./common/base.nix
   ];
+
+  system = {
+    defaults = {
+      dock = {
+        persistent-apps = [
+          "/Applications/Nix Apps/Zed.app"
+          "/Applications/Nix Apps/WezTerm.app"
+          "/Applications/Nix Apps/Google Chrome.app"
+          "/Applications/zoom.us.app/"
+        ];
+      };
+    };
+  };
+
 }
+
